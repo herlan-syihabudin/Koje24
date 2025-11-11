9c93977ffe22d31d17a7bb050c5bdeca3e50ae52
herlan-syihabudin <herlansyihabudin@gmail.com>
2025-11-12 00:35:42 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,28 @@
 import Header from "@/components/Header"
 import Hero from "@/components/Hero"
 import ProductGrid from "@/components/ProductGrid"
-import PackagesSection from "@/components/PackagesSection"
 import AboutSection from "@/components/AboutSection"
 import SubscriptionSection from "@/components/SubscriptionSection"
 import TestimoniCarousel from "@/components/TestimoniCarousel"
 import FaqSection from "@/components/FaqSection"
 import Footer from "@/components/Footer"
 import StickyCartBar from "@/components/StickyCartBar"
-
-// Popup
 import CartPopup from "@/components/CartPopup"
 import PackagePopup from "@/components/PackagePopup"
+import PackagesSection from "@/components/PackagesSection"
 
 export default function HomePage() {
   return (
     <main>
       <Header />
       <Hero />
-      <PackagesSection />
       <ProductGrid />
       <AboutSection />
+      <PackagesSection />
       <SubscriptionSection />
       <TestimoniCarousel />
       <FaqSection />
       <Footer />
-
-      {/* Komponen global (sekali render) */}
       <CartPopup />
       <PackagePopup />
       <StickyCartBar />
