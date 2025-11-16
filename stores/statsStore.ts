be0e224805193ae62dd0b210b6297c7cadcfbc56
herlan-syihabudin <herlansyihabudin@gmail.com>
2025-11-16 12:15:09 +0700

    Create statsStore.ts

---
+++ stores/statsStore.ts
@@ -0,0 +1,25 @@
+"use client"
+import { create } from "zustand"
+import { persist } from "zustand/middleware"
+
+type StatsState = {
+  orders: Record<string, number>
+  addOrder: (id: string, qty: number) => void
+}
+
+export const useStatsStore = create<StatsState>()(
+  persist(
+    (set) => ({
+      orders: {},
+
+      addOrder: (id, qty) =>
+        set((state) => ({
+          orders: {
+            ...state.orders,
+            [id]: (state.orders[id] || 0) + qty,
+          },
+        })),
+    }),
+    { name: "koje-stats" }
+  )
+)
