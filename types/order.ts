c7119ca091f1a905f8c80f4d490db4bdbe96333a
herlan-syihabudin <herlansyihabudin@gmail.com>
2025-11-16 15:19:50 +0700

    Update order.ts

--- types/order.ts
+++ types/order.ts
@@ -1,15 +1,14 @@
-// /types/order.ts
-
-export type SheetsOrder = {
+export interface SheetsOrder {
   timestamp: string
   invoiceId: string
   nama: string
   hp: string
   alamat: string
   produk: string
+  qty: number // ← TAMBAHKAN BARIS INI
   total: number
   status: string
   paymentMethod: string
   bankInfo: string
-  invoiceLink: string
+  linkInvoice: string
 }
