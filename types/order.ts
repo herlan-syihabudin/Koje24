8e59593afdd4bb1824e924d909deb48894789e29
herlan-syihabudin <herlansyihabudin@gmail.com>
2025-11-16 14:58:17 +0700

    Create order.ts

---
+++ types/order.ts
@@ -0,0 +1,15 @@
+// /types/order.ts
+
+export type SheetsOrder = {
+  timestamp: string
+  invoiceId: string
+  nama: string
+  hp: string
+  alamat: string
+  produk: string
+  total: number
+  status: string
+  paymentMethod: string
+  bankInfo: string
+  invoiceLink: string
+}
