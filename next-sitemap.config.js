3e09bc41c0b2ea6c1fb9a89ddb7932051a771614
herlan-syihabudin <herlansyihabudin@gmail.com>
2025-11-27 20:06:14 +0700

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,20 +1,23 @@
-// @ts-nocheck
+// @ts-check
 /** @type {import('next-sitemap').IConfig} */
-export default {
-  siteUrl: "https://webkoje-cacs.vercel.app", // ganti ke domain KOJE24 nanti
+
+module.exports = {
+  siteUrl: "https://webkoje-cacs.vercel.app", // ganti ke domain resmi KOJE24 nanti
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "weekly",
   priority: 0.7,
+
   exclude: [
     "/invoice/*",
     "/checkout",
     "/api/*",
   ],
+
   robotsTxtOptions: {
     policies: [
       { userAgent: "*", allow: "/" },
-      { userAgent: "*", disallow: ["/invoice/", "/checkout", "/api/"] },
+      { userAgent: "*", disallow: ["/invoice/*", "/checkout", "/api/*"] },
     ],
   },
 };
