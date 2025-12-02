7d5fbb21a3e9138f0810c36c1f283fd0c54aa5b6
herlan-syihabudin <herlansyihabudin@gmail.com>
2025-12-02 06:09:54 +0000

    install qrcode for PDF invoice generator

--- .next/server/app/api/testimonial/route.js
+++ .next/server/app/api/testimonial/route.js
@@ -1,5 +1,5 @@
 var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/testimonial/route.js")
-R.c("server/chunks/[root-of-the-server]__46e3bb1a._.js")
+R.c("server/chunks/[root-of-the-server]__c6b50464._.js")
 R.c("server/chunks/[root-of-the-server]__8f5ebbc3._.js")
 R.c("server/chunks/_next-internal_server_app_api_testimonial_route_actions_80ba6ba9.js")
 R.m(14226)
--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -128,6 +128,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../app/api/invoice-pdf/[id]/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/invoice-pdf/[id]">> = Specific
+  const handler = {} as typeof import("../../app/api/invoice-pdf/[id]/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../app/api/invoice/[id]/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/invoice/[id]">> = Specific
