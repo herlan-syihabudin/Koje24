f8d25734f00e7e088ef37b2379f5606eae69e8b2
herlan-syihabudin <herlansyihabudin@gmail.com>
2025-12-03 21:15:00 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Hero from "@/components/Hero"
 import ProductGrid from "@/components/ProductGrid"
 import AboutSection from "@/components/AboutSection"
 import SubscriptionSection from "@/components/SubscriptionSection"
-import TestimoniCarousel from "@/components/TestimoniCarousel"
+import TestimonialsCarousel from "@/components/TestimonialsCarousel"
 import FaqSection from "@/components/FaqSection"
 import Footer from "@/components/Footer"
 import CartPopup from "@/components/CartPopup"
@@ -42,7 +42,7 @@ export default function HomePage() {
 
       {/* TESTIMONI */}
       <section id="testimoni" className="scroll-mt-24">
-        <TestimoniCarousel />
+        <TestimonialsCarousel />
       </section>
 
       {/* FAQ */}
