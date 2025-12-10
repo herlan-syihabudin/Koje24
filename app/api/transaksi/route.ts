af36afc81d127aa6b28b94f1189c855bb0e82373
herlan-syihabudin <herlansyihabudin@gmail.com>
2025-12-10 12:03:22 +0700

    Update route.ts

--- app/api/transaksi/route.ts
+++ app/api/transaksi/route.ts
@@ -3,16 +3,16 @@ import { google } from "googleapis";
 
 export async function GET() {
   try {
-    const CLIENT_EMAIL = process.env.GS_CLIENT_EMAIL ?? "";
-    const PRIVATE_KEY_RAW = process.env.GS_PRIVATE_KEY ?? "";
-    const SHEET_ID = process.env.GS_SHEET_ID ?? "";
+    const CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL ?? "";
+    const PRIVATE_KEY_RAW = process.env.GOOGLE_PRIVATE_KEY ?? "";
+    const SHEET_ID = process.env.GOOGLE_SHEET_ID ?? "";
 
     const PRIVATE_KEY = PRIVATE_KEY_RAW
       .replace(/\\n/g, "\n")
       .replace(/\\\\n/g, "\n");
 
     if (!CLIENT_EMAIL || !PRIVATE_KEY || !SHEET_ID) {
-      throw new Error("Missing GS_CLIENT_EMAIL or GS_PRIVATE_KEY or GS_SHEET_ID");
+      throw new Error("Missing GOOGLE_CLIENT_EMAIL or GOOGLE_PRIVATE_KEY or GOOGLE_SHEET_ID");
     }
 
     const auth = new google.auth.GoogleAuth({
