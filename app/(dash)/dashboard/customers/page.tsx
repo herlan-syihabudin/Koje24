3912f6ffabf8394e3a67e9a3090325910af3474d
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-04 22:39:47 +0700

    Create page.tsx

---
+++ app/(dash)/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function CustomersPage() {
+  return (
+    <div className="border rounded-2xl p-5">
+      <p className="text-sm font-semibold">Pelanggan</p>
+      <p className="text-sm text-gray-600 mt-1">Placeholder</p>
+    </div>
+  );
+}
