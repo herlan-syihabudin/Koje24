eb4f63fc2bf86fe774d46d1b587ef97184cfa6e1
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-06 10:38:55 +0700

    Fix import path for SalesChart component

--- app/(dash)/dashboard/(protected)/page.tsx
+++ app/(dash)/dashboard/(protected)/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import SalesChart from "@/components/dashboard/SalesChart";
+import SalesChart from "@/components/dash/SalesChart";
 
 /* =====================
    HELPER
