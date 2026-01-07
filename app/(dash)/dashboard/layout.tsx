5b0b9afe8e37ada0b2ac6eb0c848d02365cbd8f6
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-07 09:51:16 +0700

    Update layout.tsx

--- app/(dash)/dashboard/layout.tsx
+++ app/(dash)/dashboard/layout.tsx
@@ -1,7 +1,19 @@
+import Topbar from "@/components/dash/Topbar";
+
 export default function DashboardPublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <div className="flex flex-col min-h-screen bg-[#F9FBFB]">
+      {/* TOP HEADER */}
+      <Topbar />
+
+      {/* PAGE CONTENT */}
+      <main className="px-4 md:px-8 py-6">
+        {children}
+      </main>
+    </div>
+  );
 }
