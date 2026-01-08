dc4fc4b76c8a3fbdf90406b455530d62874bf6e8
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-01-08 13:43:24 +0700

    Update layout.tsx

--- app/(dash)/layout.tsx
+++ app/(dash)/layout.tsx
@@ -2,19 +2,10 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { verifySession, getCookieName } from "@/lib/dashboardAuth";
 
-export default async function DashLayout({
+export default function DashLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const cookieStore = await cookies(); // 🔑 WAJIB await
-  const token = cookieStore.get(getCookieName())?.value;
-
-  const session = verifySession(token);
-
-  if (!session) {
-    redirect("/dashboard/login");
-  }
-
-  return children;
+  return <>{children}</>;
 }
