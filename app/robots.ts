dd023a17650d60b32f591fccb3203875d4504555
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-06-27 21:25:19 +0700

    Refactor robots.txt rules to use array format

--- app/robots.ts
+++ app/robots.ts
@@ -3,18 +3,23 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/invoice/',
-        '/checkout',
-        '/api/',
-        '/dashboard/',
-        '/pusat-bantuan/',
-        '/unsubscribe',
-      ],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/invoice/',
+          '/checkout',
+          '/api/',
+          '/dashboard/',
+          '/admin/',
+          '/unsubscribe',
+          '/_next/',
+          '/_vercel/',
+        ],
+      },
+    ],
     sitemap: 'https://koje24.com/sitemap.xml',
+    host: 'https://koje24.com',
   }
 }
