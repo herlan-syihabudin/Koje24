db89890cd48e894e4435fbdf7bd0bb809d2cee8a
herlan-syihabudin <herlansyihabudin@gmail.com>
2026-06-27 13:08:52 +0700

    Update page.tsx

--- app/pusat-bantuan/page.tsx
+++ app/pusat-bantuan/page.tsx
@@ -1,6 +1,14 @@
 // app/pusat-bantuan/page.tsx
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import HelpCenter from "@/components/HelpCenter";
 
 export default function PusatBantuanPage() {
-  return <HelpCenter />;
+  return (
+    <>
+      <Header />
+      <HelpCenter />
+      <Footer />
+    </>
+  );
 }
